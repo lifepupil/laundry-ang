f8c077ed203e75031cf1b9b728f87864aa903753
Chris Oyler <christopher.oyler@gmail.com>
2015-05-20 17:38:34 -0700

    timers working

--- client/models/machine.js
+++ client/models/machine.js
@@ -3,15 +3,17 @@
 angular.module('laundry')
 .factory('Machine', function($http, nodeUrl){
   function Machine(){}
-  
+
   Machine.checkState = function(machine){
     return $http.get(nodeUrl + '/machines/' + machine);
   };
-  
-  
-  
-  
-  
-  
+  Machine.changeState = function(machine){
+    return $http.put(nodeUrl + '/machines/' + machine);
+  };
+
+
+
+
+
   return Machine;
 });
--- client/views/general/home.js
+++ client/views/general/home.js
@@ -1,9 +1,14 @@
 'use strict';
 
 angular.module('laundry')
-.controller('Home', function($scope, $state, Machine){
-checkWashState();
-checkDryState();
+.controller('Home', function($scope, $state, Machine, $interval){
+  $interval(checkStatus, 3000);
+  checkStatus();
+  
+  function checkStatus(){
+    checkWashState();
+    checkDryState();
+  }
 
   function checkWashState(){
     Machine.checkState('washer1')
@@ -19,4 +24,13 @@ checkDryState();
       $scope.dryStatus = response.data;
     });
   }
+  $scope.changeState = function(machine){
+    console.log(machine);
+    Machine.changeState(machine)
+    .then(function(response){
+      // console.log(response);
+      // if(machine === 'dryer1')
+      checkStatus();
+    });
+  };
 });
